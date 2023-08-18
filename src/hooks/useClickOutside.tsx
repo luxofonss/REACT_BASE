055a8a9922c3ce29cd29e35895980fb533b1be3e
lux <luxofons@gmail.com>
2023-08-18 07:52:26 +0700

    init

---
+++ src/hooks/useClickOutside.tsx
@@ -0,0 +1,24 @@
+import { useEffect, RefObject } from 'react'
+
+type Callback = () => void
+
+export default function useOutsideComponent(ref: RefObject<HTMLElement>, callback: Callback = () => {}) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback()
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref, callback])
+}
